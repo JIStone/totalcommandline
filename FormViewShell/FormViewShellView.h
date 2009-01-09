b440ef596cc17bb88bcd3debde79661680629afa
orakick <orakick@93fc056e-de25-11dd-b00f-9fabb4b2ff13>
2009-01-09 08:25:32 +0000

    google 고맙다.

---
+++ FormViewShell/FormViewShellView.h
@@ -0,0 +1,113 @@
+// FormViewShellView.h : CFormViewShellView 클래스의 인터페이스
+//
+
+
+#pragma once
+#include "afxwin.h"
+#include "afxcmn.h"
+
+
+class CFormViewShellView : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CFormViewShellView();
+	DECLARE_DYNCREATE(CFormViewShellView)
+
+public:
+	enum{ IDD = IDD_FORMVIEWSHELL_FORM };
+
+// 특성입니다.
+public:
+	CFormViewShellDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+
+// 구현입니다.
+public:
+	virtual ~CFormViewShellView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedSelfile2();
+	CString m_ExecFilePath;
+//	afx_msg void OnBnClickedSelfile();
+	CString m_FullFileName;
+	afx_msg void OnBnClickedSelpath();
+	CString m_DestPath;
+//	afx_msg void OnEnChangeEdit4();
+	afx_msg void OnBnClickedCheck1();
+	CButton m_Check_EnableErrPop;
+//	afx_msg void OnBnClickedCheck2();
+	//CButton m_Check_Bmp;
+	afx_msg void OnBnClickedCheck3();
+	//CButton m_ExtName;
+	afx_msg void OnBnClickedExecappl();
+	int m_SubDirCnt;
+	int m_SearchedFileCnt;
+	// 툴팁
+	//CToolTipCtrl m_tooltip;
+
+	int SearchDir(CString sDirName, CString *sDirNameList);
+	int SearchFile(CString sDirName, CString sFileName,CString *sFileNameList);
+	int SearchOneFile(CString sDirName, CString sFileName, CString *sFileNameList);
+	// // 커맨드 명령 표시
+	void DisplayCommand(BOOL modifyed = TRUE);
+	CProgressCtrl m_ProgressCtrl;
+//	afx_msg void OnBnClickedOk();
+	afx_msg void OnEnChangePath();
+	afx_msg void OnEnChangeSrcFile();
+	afx_msg void OnEnChangeExecFile();
+	afx_msg void OnEnChangeEdit3();
+	afx_msg void OnEnChangeEdit4();
+	// 프로그래스카운트 표시
+	CEdit m_EditProgCnt;
+	afx_msg void OnNMCustomdrawProgress1(NMHDR *pNMHDR, LRESULT *pResult);
+//	afx_msg void OnEnChangeEdit2();
+//	CButton m_Check_WildDotExt;
+	CString m_ExecFileName;
+//	afx_msg void OnBnClickedCheck4();
+	CString m_PreCmdOptStr;
+	afx_msg void OnEnChangeEdit5();
+	CString m_AllCmdLnText;
+	BOOL m_FirstLoaded;
+//	afx_msg void OnBnClickedOk();
+//	CString m_ListData;
+	CListBox m_ListBox;
+	afx_msg void OnBnClickedBtnOutAdd();
+	afx_msg void OnBnClickedBtnOutDel();
+	CString m_ListData;
+//	afx_msg void OnLbnSelchangeListOut();
+	afx_msg void OnBnClickedSelfile();
+
+	CToolTipCtrl m_toolTip;
+	BOOL PreTranslateMessage(MSG* pMsg);
+	bool m_IsToolTipInit;
+	// 확장자 추가시 타입 결정
+	int m_AddExtTypeRadio;
+
+	afx_msg void OnBnClickedRadio1Move();
+	afx_msg void OnBnClickedRadio2Copy();
+	afx_msg void OnBnClickedRadio3Delete();
+};
+
+#ifndef _DEBUG  // FormViewShellView.cpp의 디버그 버전
+inline CFormViewShellDoc* CFormViewShellView::GetDocument() const
+   { return reinterpret_cast<CFormViewShellDoc*>(m_pDocument); }
+#endif
+
---
+++ FormViewShell/resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FormViewShell.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDD_FORMVIEWSHELL_FORM          101
+#define IDR_MAINFRAME                   128
+#define IDR_FormViewShellTYPE           129
+#define IDC_SRC_FILE                    1000
+#define IDC_PATH                        1001
+#define IDC_SELFILE                     1002
+#define IDC_SELPATH                     1003
+#define IDC_EXECAPPL                    1004
+#define IDC_EDIT3                       1005
+#define IDC_EXEC_FILE                   1006
+#define IDC_SELFILE2                    1007
+#define IDC_EDIT4                       1008
+#define IDC_CHECK1                      1009
+#define IDC_CHECK2                      1010
+#define IDC_CHECK3                      1011
+#define IDC_EDIT2                       1012
+#define IDC_EDIT1                       1013
+#define IDC_PROGRESS1                   1014
+#define IDC_EDIT5                       1019
+#define IDC_LIST1                       1021
+#define IDC_LIST_OUT                    1021
+#define IDC_BTN_OUT_ADD                 1022
+#define IDC_BTN_OUT_DEL                 1023
+#define IDC_EDIT_OUT                    1024
+#define IDC_RADIO1_MOVE                 1031
+#define IDC_RADIO2_COPY                 1032
+#define IDC_RADIO3                      1033
+#define IDC_RADIO3_DELETE               1033
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1034
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
